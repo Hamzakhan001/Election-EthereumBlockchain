fc5bc42a963920442498c14bb1a4c9a9ddcae999
Hamzakhan001 <58425510+Hamzakhan001@users.noreply.github.com>
2022-07-16 16:51:47 +0500

    voting

---
+++ contracts/Election.sol
@@ -0,0 +1,23 @@
+contract Election { 
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,4 @@
+var Election = artifacts.require('./Election.sol');
+
+module.exports = function (deployer) {  
+deployer.deploy(Election); };
